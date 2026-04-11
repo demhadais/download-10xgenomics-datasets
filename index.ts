24a2334e97efbcecb04fdd0a338a18603d8fc4f9
Ahmed Said <ahmed.said2290@icloud.com>
2026-04-10 22:49:40 -0400

    foo

--- index.ts
+++ index.ts
@@ -1,40 +1,31 @@
-import { chromium, type Page } from "playwright";
+import { webkit, type Page } from "playwright";
 
 async function clickSoftwareCheckbox(page: Page, softwareName: string) {
   const label = page
     .locator("label")
     .filter({ has: page.getByText(softwareName, { exact: true }) });
-  const checkbox = label.getByRole("checkbox");
-  const innerHtml = await label.innerHTML();
-  console.log(innerHtml);
-
-  // console.log("we found the label");
-  // await label.click();
-  // console.log("we clicked");
-  // const table = page.getByRole("table");
-  // table.waitFor({ state: "visible" });
-  // console.log("did we wait for table?");
 
   const rows = await page.locator("td").all();
+  if (rows.length === 50) {
+    console.log("50 rows!");
+  }
+
   for (const r of rows) {
-    console.log(await r.allInnerTexts());
+    console.log(await r.innerHTML());
   }
 }
 
 async function downloadAllDatasets() {
-  const browser = await chromium.launch();
+  const browser = await webkit.launch();
   const context = await browser.newContext();
   const page = await context.newPage();
-
   await page.goto("https://www.10xgenomics.com/datasets");
-
   const softwares = [
     "Cell Ranger",
     "Cell Ranger ATAC",
     "Cell Ranger ARC",
     "Space Ranger",
   ];
-
   for (const softwareName of softwares) {
     await clickSoftwareCheckbox(page, softwareName);
   }
